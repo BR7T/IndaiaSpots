b7a96d91918c5e20807173da3bc05569463b3cee
Joaov511 <jv232479@gmail.com>
2023-08-31 22:44:33 -0300

    database connection added

--- server.js
+++ server.js
@@ -1,15 +1,58 @@
+const { response } = require('express');
 const express = require('express');
 const port = 3060;
 const app = express();
+const mysql = require('mysql2');
 
 app.use(express.json());       
 app.use(express.urlencoded({     
   extended: true
 }));
 
-app.get('authUser', function(req,res) {
-    
-})
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+
+const con = mysql.createConnection({
+    host : "localhost",
+    user : "root",
+    password : "1234",
+    database : "IndaiaSpots"
+});
+
+function mySqlConnection() {
+    con.connect(function(err) {
+        console.log("Connection to database Successful");
+    });
+}
+
+mySqlConnection();
+
+function mySqlQueryInsert(mySqlquery,data) {
+    const query = mySqlquery;
+    const values = [data];
+    con.query(query,values,function(err,result) {
+        if(err) {throw err}
+        console.log("Query Successful");
+    });
+}
+
+app.get('/checkUser', function(req,res) {
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query("SELECT * FROM user", function (err, result, fields) {
+          if (err) throw err;
+          console.log(result);
+        });
+    })
+});
+
+
 
 app.post('/login', function(req,res) {
     const userLogin = {
@@ -21,12 +64,9 @@ app.post('/login', function(req,res) {
 
 
 app.post('/signup', (req,res) => {
-    const user = {
-        username : req.body.username,
-        email : req.body.email,
-        password : req.body.password
-    }
+    const user = [req.body.email,req.body.username,req.body.password];
     res.status(204).send();
+    mySqlQueryInsert("insert into user (email,userName,password) values(?)",user);
 });
 
 
