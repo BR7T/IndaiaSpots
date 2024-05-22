2d68129e55686dd5e513864b8f1201d13b560c6b
Joaov511 <jv232479@gmail.com>
2024-05-22 17:14:25 -0300

    add send username and email in checkToken function

--- functions/src/middleware/firebase/auth.ts
+++ functions/src/middleware/firebase/auth.ts
@@ -1,26 +1,10 @@
 import admin from 'firebase-admin';
-import { Request, Response, NextFunction } from 'express';
 const firebaseCredentials = require("../../../serviceAccountKey.json");
+import { NextFunction, Request, Response} from "express";
 
 admin.initializeApp({
     credential : admin.credential.cert(firebaseCredentials)
 })
-
-export async function firebaseAppCheck(req: Request, res: Response ,next : NextFunction) {
-    if(!req.headers["x-firebase-appcheck"]) {
-        res.status(401).send('Unathorized');
-    }
-    else {
-        const appCheckToken : any = req.headers["x-firebase-appcheck"];
-        try {
-          await admin.appCheck().verifyToken(appCheckToken);
-          next();
-        } catch (error) {
-          console.error("Error verifying token:", error);
-          res.status(401).send("Unauthorized");
-        }
-    }
-}
 
 export async function checkGoogleToken(token : string) : Promise<any> {
     await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${token}`, {
