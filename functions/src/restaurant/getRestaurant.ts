e99eff6ab63b273617b9d69d5d31bcd9654506ac
unknown <jv232479@gmail.com>
2024-06-03 22:45:07 -0300

    refactor request to get restaurants from database

--- functions/src/restaurant/getRestaurant.ts
+++ functions/src/restaurant/getRestaurant.ts
@@ -2,7 +2,7 @@ import { NextFunction } from "express";
 import { QueryError } from "mysql2";
 
 export function getRestaurant(mysqlCon : any, restaurantId : string) : Promise<JSON[]> {
-    const getRestaurantQuery = 'select * from Restaurante where ID_Restaurante = ?';
+    const getRestaurantQuery = 'select r.Nome, r.ID_Restaurante ,r.Dia_Atendimento, r.Horario_Atendimento, r.icone,r.Tipo_Cozinha, e.Bairro, e.Rua, e.Numero, i.Url from Restaurante r inner join Imagem i on r.ID_Usuario = i.ID_Restaurante inner join Endereco e on r.ID_Usuario = e.ID_Restaurante where r.ID_Restaurante=?';
     return new Promise((resolve, reject) => {
         mysqlCon.query(getRestaurantQuery,[restaurantId], (err : QueryError | null,results : JSON[] | PromiseLike<JSON[]>) => {
             if(err) reject(err);
@@ -12,7 +12,7 @@ export function getRestaurant(mysqlCon : any, restaurantId : string) : Promise<J
 }
 
 export function getAllRestaurants(mysqlCon : any , next : NextFunction) : Promise<Array<JSON>> {
-    const getAllRestaurantQuery = 'select * from Restaurante ';
+    const getAllRestaurantQuery = 'select r.Nome,r.ID_Restaurante ,r.Dia_Atendimento, r.Horario_Atendimento, r.icone, r.Tipo_Cozinha, e.Bairro, e.Rua, e.Numero, i.Url from Restaurante r inner join Imagem i on r.ID_Usuario = i.ID_Restaurante inner join Endereco e on r.ID_Usuario = e.ID_Restaurante';
     return new Promise((resolve, reject) => {
         mysqlCon.query(getAllRestaurantQuery, (err : string, results : Array<any>) => {
             if (err) {
